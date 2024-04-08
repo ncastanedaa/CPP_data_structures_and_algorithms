22b871c4ee8cc0c51d5cf1a23c51b3efbff96b57
Nicolas Castaneda <nicolas.castaneda@blacksquare.com.co>
2024-04-07 20:23:11 -0500

    correccion de la funcion fill_weghts()

--- graph.cpp
+++ graph.cpp
@@ -1,11 +1,27 @@
-//Code made by Nicolas Castaneda 
+/*
+Code made by Nicolas Castaneda
+
+This code makes a random undirected graph wth parameters #nodes and density (probability) free to the user.
+It also has the function fill_weights(...) to put random POSITIVE weigths iN the edges of the nodes.
+*/
+
 #include<iostream>
 #include<cstdlib>
 #include<vector>
 #include<random>
 #include<ctime>
 #include<conio.h>
+
 using namespace std;
+
+
+class Node{
+// this class represents the Node element with an int index and a list of adjacent nodes to it.
+public:
+
+private:
+
+};
 
 class Graph{
 
@@ -13,18 +29,18 @@ class Graph{
 
 public:
 
-int nodes;
+int number_of_nodes;
 double density;
 vector<vector<int>> matrix;
 
-Graph(int nd,double dns):nodes(nd),density(dns), matrix(make_unweighted_graph(nd,dns)){
+Graph(int nd,double dns):number_of_nodes(nd),density(dns), matrix(make_unweighted_graph(nd,dns)){
     
 }
 
 vector<vector<int>> fill_weights(vector<vector<int>>& matrix,int max_weight){
     for(int row = 0; row < matrix.size(); row++){
-        for(int column = 0; column < matrix.size(); column++){
-            matrix[row][column] *= randomInt(max_weight);
+        for(int column = 0; column < row; column++){
+            matrix[row][column] = matrix[column][row] *= randomInt(max_weight);
         }
     }
     return matrix;
@@ -44,13 +60,13 @@ void printg(vector<vector<int>>& matrix){
 private:
 
 vector<vector<int>> make_unweighted_graph(int num_nodes, double probability){
-    num_nodes = nodes;
+    num_nodes = number_of_nodes;
     probability = density;
 
-    vector<vector<int>> matrix(nodes,vector<int>(nodes,0));
+    vector<vector<int>> matrix(number_of_nodes,vector<int>(number_of_nodes,0));
 
-    for(int row = 0; row<nodes; row++){
-        for(int column = 0; column < nodes; column++){
+    for(int row = 0; row<number_of_nodes; row++){
+        for(int column = 0; column < number_of_nodes; column++){
             if(row == column){
                 matrix[row][column] = 0;
             }else{
@@ -63,7 +79,7 @@ vector<vector<int>> make_unweighted_graph(int num_nodes, double probability){
 };
 
 
-int generateRandomOnes(double probability){
+inline int generateRandomOnes(double probability){
     double percentile = probability*100;
     int random = rand()%100+1;
     if(random <= percentile){
@@ -73,18 +89,17 @@ int generateRandomOnes(double probability){
     }
 }  
 
-int randomInt(int max){
+inline int randomInt(int max){
     int random = rand()%max+1;
     return random;
 }
-
 
 };
 
 
 int main(){
     srand(time(NULL));
-    Graph my_graph_1(50,0.1);
+    Graph my_graph_1(6,0.3);
     my_graph_1.printg(my_graph_1.matrix);
     cout << endl;
     my_graph_1.fill_weights(my_graph_1.matrix,9);
